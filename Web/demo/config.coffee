75ea1ba84f31ee756420c76352dde557912521e9
Erik Peterson <cerikpete@gmail.com>
2014-04-13 14:53:52 -0400

    Initial commit

---
+++ Web/demo/config.coffee
@@ -0,0 +1,20 @@
+﻿app = angular.module 'parkApp', ['ngRoute']
+app.config ['$routeProvider', ($routeProvider) ->
+	$routeProvider
+		.when '/',
+			controller: 'AttractionCtrl',
+			templateUrl:'views/attractions.html'		
+		.when '/parks',
+			templateUrl:'views/parks.html'		
+		.otherwise
+			redirectTo: '/'
+]
+
+app.factory 'AttractionFactory', ->
+	@getAttractions = ->
+		return [{ Name: 'Space Mountain' }, { Name: 'Ariel' }, { Name: 'Star Tours' }]
+	@
+
+app.controller 'AttractionCtrl', ['$scope', 'AttractionFactory', ($scope, factory) ->
+	$scope.Attraction = factory.getAttractions();	
+]
---
+++ Web/demo/config.js
@@ -0,0 +1,42 @@
+﻿(function() {
+  var app;
+
+
+
+  app = angular.module('parkApp', ['ngRoute']);
+
+  app.config([
+    '$routeProvider', function($routeProvider) {
+      return $routeProvider.when('/', {
+        controller: 'AttractionCtrl',
+        templateUrl: 'views/attractions.html'
+      }).when('/parks', {
+        templateUrl: 'views/parks.html'
+      }).otherwise({
+        redirectTo: '/'
+      });
+    }
+  ]);
+
+  app.factory('AttractionFactory', function() {
+    this.getAttractions = function() {
+      return [
+        {
+          Name: 'Space Mountain'
+        }, {
+          Name: 'Ariel'
+        }, {
+          Name: 'Star Tours'
+        }
+      ];
+    };
+    return this;
+  });
+
+  app.controller('AttractionCtrl', [
+    '$scope', 'AttractionFactory', function($scope, factory) {
+      return $scope.Attraction = factory.getAttractions();
+    }
+  ]);
+
+}).call(this);
